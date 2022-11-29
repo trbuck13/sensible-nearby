3379294d74302bafd912c48eb506a0773cc9bc0f
Ryan <trbuck13@gmail.com>
2022-11-29 17:33:09 -0600

    add more styles

--- components/NearbyWidget/ResultsItem.tsx
+++ components/NearbyWidget/ResultsItem.tsx
@@ -2,13 +2,33 @@ import styles from "./Widget.module.css";
 const ResultItem = ({
   result,
 }: {
-  result: { name: string; address: string; rating: number };
+  result: {
+    name: string;
+    vicinity: string;
+    rating: number;
+    icon: string;
+    icon_background_color: string;
+  };
 }) => {
   return (
     <div className={styles.resultItem}>
-      <div className="result-item__name">{result.name}</div>
-      <div className="result-item__address">{result.address}</div>
-      <div className="resultItemAddress">{result.rating}</div>
+      <div className={styles.spaceBetween}>
+        <div>
+          <div className={styles.resultItemName}>
+            <img
+              src={result.icon}
+              alt=""
+              aria-hidden="true"
+              className={styles.resultIcon}
+            />
+            <div>
+              <div>{result.name}</div>
+              <div className={styles.resultItemAddress}>{result.vicinity}</div>
+            </div>
+          </div>
+        </div>
+        <div className="resultItemRating">{result.rating}</div>
+      </div>
     </div>
   );
 };
