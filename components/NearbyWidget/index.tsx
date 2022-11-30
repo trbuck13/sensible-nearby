46e26c7a8f5706ca2f600269b5fd07c7649e428c
Ryan <trbuck13@gmail.com>
2022-11-29 18:00:40 -0600

    remove unused package

--- components/NearbyWidget/index.tsx
+++ components/NearbyWidget/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import LocationSelectInput from "./LocationSelectInput";
 import ResultsList from "./ResultsList";
 import TextInput from "./TextInput";
