a0aa80f2a3ebb2e7345d59753be7b8c174430c0e
Ryan <trbuck13@gmail.com>
2022-11-29 18:08:58 -0600

    cleanup change handler

--- components/NearbyWidget/TextInput.tsx
+++ components/NearbyWidget/TextInput.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import styles from './Widget.module.css';
+import styles from "./Widget.module.css";
+
 const TextInput = ({
   value,
   onChange,
--- components/NearbyWidget/index.tsx
+++ components/NearbyWidget/index.tsx
@@ -5,25 +5,23 @@ import TextInput from "./TextInput";
 import styles from "./Widget.module.css";
 
 const NearbyWidget = () => {
-  const [location, setLocation] = useState("39.2130,-106.9378");
-  const [search, setSearch] = useState("");
+  const [form, setForm] = useState({
+    location: "39.2130,-106.9378",
+    keyword: "",
+  });
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const handleLocationChange = (e: React.BaseSyntheticEvent) => {
-    setLocation(e.target.value);
-  };
-
-  const handleSearchChange = (e: React.BaseSyntheticEvent) => {
-    setSearch(e.target.value);
+  const handleChange = (e: React.BaseSyntheticEvent) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
   };
 
   const fetchResults = async () => {
     await setLoading(true);
     const response = await fetch(
       `/api/maps/api/place/nearbysearch/json?location=${encodeURIComponent(
-        location
-      )}&rankby=distance&keyword=${search}&key=${
+        form.location
+      )}&rankby=distance&keyword=${form.keyword}&key=${
         process.env.NEXT_PUBLIC_NEARBY_API_KEY
       }`,
       { cache: "no-cache" }
@@ -39,17 +37,18 @@ const NearbyWidget = () => {
   };
 
   useEffect(() => {
-    if (!search) {
+    if (!form.keyword) {
       return;
     }
     fetchResults();
-  }, [location, search]);
+  }, [form]);
 
   return (
     <div className={styles.widgetContainer}>
       <div className={styles.widgetForm}>
-        <LocationSelectInput onChange={handleLocationChange} />
-        <TextInput value={search} onChange={handleSearchChange} />
+        <LocationSelectInput onChange={handleChange} />
+        {/* TODO: throttle/debounce this onchange event */}
+        <TextInput value={form.keyword} onChange={handleChange} />
       </div>
       <ResultsList loading={loading} results={results} />
     </div>
